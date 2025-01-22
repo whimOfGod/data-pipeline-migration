6a674ad94f71389c1dfd01bc2c8d98122038bdc9
whimofgod <aaronkolins98@gmail.com>
2025-01-22 23:23:00 +0100

    adding network_data_broker

---
+++ network_data_broker_function/kafka_broker_instance/eNDBF.py
@@ -0,0 +1,37 @@
+# Python script to start the Kafka server and ZooKeeper server
+
+import os
+import subprocess
+import signal
+import sys
+
+
+
+
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) < 2:
+        print("Usage: python3 eNDBF.py <kafka_dir>")
+        sys.exit(1)
+    kafka_dir = str(sys.argv[1])
+    os.chdir(kafka_dir)
+    
+    # Start ZooKeeper
+    zookeeper_process = subprocess.Popen(["bin/zookeeper-server-start.sh", "config/zookeeper.properties"])
+    
+    # Start Kafka Server
+    kafka_process = subprocess.Popen(["bin/kafka-server-start.sh", "config/server.properties"])
+    
+    # Wait for user input to stop Kafka
+    input("Kafka server is running. Press Enter to stop.")
+    
+    # Stop Kafka Server
+    kafka_process.send_signal(signal.SIGTERM)
+    kafka_process.wait()
+    
+    # Stop ZooKeeper Server
+    zookeeper_process.send_signal(signal.SIGTERM)
+    zookeeper_process.wait()
+    
+    print("Kafka server stopped.")
